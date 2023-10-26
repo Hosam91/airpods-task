97f81b6a32794fe50c12ba5dfc427f89f2ec3445
Hosam91 <99843529+Hosam91@users.noreply.github.com>
2023-10-26 17:45:30 +0300

    handle umgs scale

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // images elements 
-const firstPic = document.querySelector(".first");
-const secondPic = document.querySelector(".second");
-const thirdPic = document.querySelector(".third");
-const rightText = document.querySelector(".rightText");
+// const firstPic = document.querySelector(".first");
+const firstPic = document.querySelector(".first img");
+const secondPic = document.querySelector(".second img");
+const thirdPic = document.querySelector(".third img");
 
 // text elements
 
 const firstText= document.querySelector(".f-text");
 const secondText= document.querySelector(".s-text");
+const rightText = document.querySelector(".rightText");
 
 
 document.addEventListener('scroll', handleScroll)
@@ -17,9 +18,8 @@ function handleScroll()
     const scrollY = window.scrollY;
     let opacity = 0;
     opacity = scrollY / 820
-
-
- 
+    // let newSize = 100 - scrollY * 0.1;
+    let newSize = 100- scrollY * 0.01;
     //showing default picture and setting other pic's opacity to 0 
     if (scrollY === 0 )
     {
@@ -33,16 +33,21 @@ function handleScroll()
     }
     
     //changing opacity and showing the second picture and  hiding other picturs
-    if ( opacity > 0  && opacity <= 1  )
+    if ( opacity => 0  && opacity < 1  )
     {
-        secondPic.style.opacity = opacity
+        firstPic.style.opacity = 1 - opacity
         thirdPic.style.opacity = 0
+        secondPic.style.opacity = 0
+        //handle picture scale
+
+        firstPic.style.transform = `scale(${newSize/100})`;
+        secondPic.style.transform = `scale(${newSize/100})`;
+        // firstPic.style.transform = `scale(${newSize/100})`;
+
 
         //handle text opacity with first picture
        //handeld on scroll Y value
-        
         firstText.style.opacity = opacity + .6
-
         if (scrollY>=120 && scrollY<210)
         {
             firstText.style.opacity= 1
@@ -51,13 +56,20 @@ function handleScroll()
         if (scrollY > 210)
         {
             firstText.style.opacity = 0
-            secondText.style.opacity= opacity+.4
+            secondText.style.opacity = opacity + .4
+            rightText.style.opacity = 0
+
         }
         if (scrollY > 620)
         {
+            firstPic.style.opacity= 0
             firstText.style.opacity = 0
             secondText.style.opacity= 0
-            rightText.style.opacity= 1
+        }
+        if (scrollY > 640)
+        {
+            rightText.style.opacity = 1
+            secondPic.style.opacity = opacity -.4
         }
         if (scrollY > 800)
         {
@@ -65,9 +77,6 @@ function handleScroll()
             secondText.style.opacity= 0
             rightText.style.opacity= 0
         }
-        
-           
-        
 }
     //changing opacity and showing the third picture
     // hide other picturs
